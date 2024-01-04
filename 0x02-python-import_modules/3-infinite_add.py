6a4e9f2720c769f7ce1b66021dd8c86296a266ae
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-04 23:05:19 +0200

    3-infinite_add.py no_execute on expot

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-i = 1
-sum = 0
-while (i < len(argv)):
-    sum += eval(argv[i])
-    i += 1
-print(sum)
+    i = 1
+    sum = 0
+    while (i < len(argv)):
+        sum += eval(argv[i])
+        i += 1
+    print(sum)
