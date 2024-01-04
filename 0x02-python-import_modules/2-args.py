5801a6e16639725459c9b316647db5b6dc8e4234
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-04 18:02:30 +0200

    changed import to from xx import yy

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
     i = 1
-    arg = "argument" if len(sys.argv) == 2 else "arguments"
-    sep = ":" if len(sys.argv) > 1 else "."
+    arg = "argument" if len(argv) == 2 else "arguments"
+    sep = ":" if len(argv) > 1 else "."
 
-    print("{} {}{}".format(len(sys.argv) - 1, arg, sep))
-    while (i < len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+    print("{} {}{}".format(len(argv) - 1, arg, sep))
+    while (i < len(argv)):
+        print("{}: {}".format(i, argv[i]))
         i += 1
