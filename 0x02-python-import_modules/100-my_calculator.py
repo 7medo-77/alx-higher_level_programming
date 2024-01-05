4b59033f50f4683fdf306024d166554050c7d2ae
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-05 04:17:39 +0200

    100-my_calculator.py_2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv, exit
     import calculator_1
 
     i = 1
     signArray = "+-/*"
     res = 0
 
-    if (len(sys.argv) != 4):
+    if (len(argv) != 4):
         print(
             "Usage: ./100-my_calculator.py <a> <operator> <b>"
         )
-        sys.exit(1)
+        exit(1)
 
-    match sys.argv[2]:
+    match argv[2]:
         case "+":
-            res = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+            res = calculator_1.add(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
         case "-":
-            calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+            calculator_1.sub(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
         case "/":
-            calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+            calculator_1.div(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
         case "*":
-            calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+            calculator_1.mul(int(argv[1]), int(argv[3]))
+            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
         case other:
             print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+            exit(1)
