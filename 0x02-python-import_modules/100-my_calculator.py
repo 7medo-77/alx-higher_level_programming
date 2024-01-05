454999b994b9e27a67fb10cf96c3a1695b93d428
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-05 04:06:38 +0200

    100-my_calculator.py_2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-import sys
-import calculator_1
+if __name__ == "__main__":
+    import sys
+    import calculator_1
 
-i = 1
-signArray = "+-/*"
-res = 0
+    i = 1
+    signArray = "+-/*"
+    res = 0
 
-if (len(sys.argv) != 4):
-    print(
-        "Usage: ./100-my_calculator.py <a> <operator> <b>"
-    )
-    sys.exit(1)
-
-match sys.argv[2]:
-    case "+":
-        res = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
-    case "-":
-        calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
-    case "/":
-        calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
-    case "*":
-        calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-        print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
-    case other:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if (len(sys.argv) != 4):
+        print(
+            "Usage: ./100-my_calculator.py <a> <operator> <b>"
+        )
         sys.exit(1)
+
+    match sys.argv[2]:
+        case "+":
+            res = calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+        case "-":
+            calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+        case "/":
+            calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+        case "*":
+            calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+        case other:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
