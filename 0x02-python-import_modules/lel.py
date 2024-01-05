19abe8945fade6d9fa9b5ca0c8dda89d11441c86
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-05 04:37:16 +0200

    100-my_calculator.py_8

---
+++ 0x02-python-import_modules/lel.py
@@ -0,0 +1,32 @@
+
+# #!/usr/bin/python3
+# if __name__ == "__main__":
+#     from sys import argv, exit
+#     from calculator_1 import add, sub, mul, div
+#
+#     i = 1
+#     signArray = "+-/*"
+#     res = 0
+#
+#     if (len(argv) != 4):
+#         print(
+#             "Usage: ./100-my_calculator.py <a> <operator> <b>"
+#         )
+#         exit(1)
+#
+#     match argv[2]:
+#         case "+":
+#             res = add(int(argv[1]), int(argv[3]))
+#             print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+#         case "-":
+#             res = sub(int(argv[1]), int(argv[3]))
+#             print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+#         case "/":
+#             res = div(int(argv[1]), int(argv[3]))
+#             print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+#         case "*":
+#             res = mul(int(argv[1]), int(argv[3]))
+#             print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+#         case other:
+#             print("Unknown operator. Available operators: +, -, * and /")
+#             exit(1)
