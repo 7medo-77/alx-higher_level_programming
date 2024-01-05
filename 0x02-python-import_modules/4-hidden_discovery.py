06cbc5e1802d07c4ea97011ddca46e646abb81ba
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-05 03:07:17 +0200

    5-variable_load.py_4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-for a in range(dir(hidden_4)):
-    if (a[:2] != "__"):
-        print(a)
+if __name__ == "__main__":
+    import hidden_4
+    for a in range(dir(hidden_4)):
+        if (a[:2] != "__"):
+            print(a)
