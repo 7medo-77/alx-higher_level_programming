172eefb42471425f9021120d85d201a66bbed394
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-08 16:46:51 +0200

    2-replace_in_list.py_2

--- 0x02-python-import_modules/lel.py
+++ 0x02-python-import_modules/lel.py
@@ -1,32 +1,29 @@
-
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv, exit
+    import sys
     from calculator_1 import add, sub, mul, div
 
     i = 1
     signArray = "+-/*"
     res = 0
 
-    if (len(argv) != 4):
-        print(
-            "Usage: ./100-my_calculator.py <a> <operator> <b>"
-        )
-        exit(1)
+    if (len(sys.argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    match argv[2]:
+    match sys.argv[2]:
         case "+":
-            res = add(int(argv[1]), int(argv[3]))
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+            res = add(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
         case "-":
-            res = sub(int(argv[1]), int(argv[3]))
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+            res = sub(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
         case "/":
-            res = div(int(argv[1]), int(argv[3]))
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+            res = div(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
         case "*":
-            res = mul(int(argv[1]), int(argv[3]))
-            print("{} {} {} = {}".format(argv[1], argv[2], argv[3], res))
+            res = mul(int(sys.argv[1]), int(sys.argv[3]))
+            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
         case other:
             print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+            sys.exit(1)
