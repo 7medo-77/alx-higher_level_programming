78594da5f8cbe86f416bc195e1543a6041431cbd
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-09 04:36:57 +0200

    7-add_tuple.py_5

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,7 +7,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     ):
         res.append(
             (0 if i >= len(tuple_a) else tuple_a[i]) +
-            (0 if i >= len(tuple_b) else tuple_b[i]))
-    new_tuple = tuple(i for i in res)
+            (0 if i >= len(tuple_b) else tuple_b[i])
+        )
+    new_tuple = tuple(res)
 
     return (new_tuple[:2])
