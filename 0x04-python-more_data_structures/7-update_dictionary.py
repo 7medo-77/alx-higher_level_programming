2b0935dc3f14f8d1b4b52f0dba7a3d292bdaf114
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-11 02:14:05 +0200

    7-update_dictionary.py_2

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
-
-    # if key in a_dictionary:
-    #     a_dictionary[key] = value 
-    # else:
-    #     a_dictionary[key] = value 
+    key_string = str(key)
+    a_dictionary[key_string] = value
