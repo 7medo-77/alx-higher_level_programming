cd5748e66ae34c215ef828a09354dae2182b8516
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-11 18:43:01 +0200

    12-roman_to_int.py_1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,19 @@
 def roman_to_int(roman_string):
     if (not roman_string):
         return (0)
+
     roman_dict = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
     rolling_sum = 0
-    for char in roman_string:
+    for index, char in enumerate(roman_string):
         for key, value in roman_dict.items():
             if (char == key):
-                rolling_sum += value
+                if (key == 'I' and index + 1 < len(roman_string)):
+                    if (roman_string[index + 1] == 'V' or roman_string[index + 1] == 'X'):
+                        rolling_sum += (value * -1)
+                    else:
+                        rolling_sum += value
+                else:
+                    rolling_sum += value
     return (rolling_sum)
