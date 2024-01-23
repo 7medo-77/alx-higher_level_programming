7c467e3348501720a37500cc0bd495c7820048b5
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-23 21:08:55 +0200

    2-square.py_1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,19 @@
-
 #!/usr/bin/python3
 """Module to instantiate a class"""
 
 
 class Square:
     """
-    Function to instantiate a class Square, 
-    with optional size attribute and with 
+    Function to instantiate a class Square,
+    with optional size attribute and with
     error handling
     """
 
     def __init__(self, size=0):
         try:
             self.__size = size
+            if size is None:
+                size = 0
         except TypeError:
             print("size must be an integer")
         except ValueError:
