89d33dcf2987d8654d41cc2ff69b53dd18a90ca3
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-23 23:03:02 +0200

    4-square.py_0

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Module to instantiate a class"""
+
+
+class Square:
+    """
+    Function to instantiate a class Square,
+    with optional size attribute and with
+    error handling
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise ValueError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    """
+    Getter function to get the value of size
+    """
+    def size(self):
+        return (self.__size)
+
+    """
+    Setter function to set the value of size
+    """
+    def size(self, value):
+        self.__size = value
+        if not isinstance(self.__size, int):
+            raise ValueError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    """
+    Function to calculate the area of a square instance
+    """
+    def area(self):
+        return (self.__size ** 2)
