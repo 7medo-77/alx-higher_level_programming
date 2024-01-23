7eeac68f681e33f870a69587c77c3c1709d97e38
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-23 23:55:46 +0200

    4-square.py_1

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,20 +11,18 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
-        if not isinstance(self.__size, int):
-            raise ValueError("size must be an integer")
-        if self.__size < 0:
-            raise ValueError("size must be >= 0")
 
     """
     Getter function to get the value of size
     """
+    @property
     def size(self):
         return (self.__size)
 
     """
     Setter function to set the value of size
     """
+    @size.setter
     def size(self, value):
         self.__size = value
         if not isinstance(self.__size, int):
