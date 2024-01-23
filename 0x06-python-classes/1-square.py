8c4c1c50c3545263403e8aec986f85f70882cbfc
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-23 20:56:35 +0200

    1-square.py_0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module to instantiate a class"""
+
+
+class Square:
+    """
+    Function to instantiate a class Square
+    """
+
+    def __import__(self, size):
+        self.__size = size
