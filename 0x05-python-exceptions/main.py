80edd7c178e82a111044e10db3f6e33023044d67
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-23 04:21:51 +0200

    3-print_division

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,17 +1,60 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+safe_print_division = __import__('3-safe_print_division').safe_print_division
 
-value = 89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
 
-value = -89
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
 
-value = "School"
-has_been_print = safe_print_integer(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
+# safe_print_list_integers = \
+#     __import__('2-safe_print_list_integers').safe_print_list_integers
+#
+# my_list = [1, 2, 3, 4, 5]
+#
+# nb_print = safe_print_list_integers(my_list, 2)
+# print("nb_print: {:d}".format(nb_print))
+#
+# my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+# nb_print = safe_print_list_integers(my_list, len(my_list))
+# print("nb_print: {:d}".format(nb_print))
+#
+# nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+# print("nb_print: {:d}".format(nb_print))
+#
+# Task2
+# safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+#
+# value = 89
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
+#
+# value = -89
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
+#
+# value = "School"
+# has_been_print = safe_print_integer(value)
+# if not has_been_print:
+#     print("{} is not an integer".format(value))
+
+
+#task1
+# #!/usr/bin/python3
+# safe_print_list = __import__('0-safe_print_list').safe_print_list
+#
+# my_list = [1, 2, 3, 4, 5]
+#
+# nb_print = safe_print_list(my_list, 2)
+# print("nb_print: {:d}".format(nb_print))
+# nb_print = safe_print_list(my_list, len(my_list))
+# print("nb_print: {:d}".format(nb_print))
+# nb_print = safe_print_list(my_list, len(my_list) + 2)
+# print("nb_print: {:d}".format(nb_print))
+#
