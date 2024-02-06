e41ec8cdda11bf8d26544007e4220084192f2142
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-07 01:14:31 +0200

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ Function that reads a text file and prints it to stdout
 def read_file(filename=""):
     with open(filename) as file_1:
         read_file = file_1.read()
-        print(read_file)
+        print(read_file, end="")
