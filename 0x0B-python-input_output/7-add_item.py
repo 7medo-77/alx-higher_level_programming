0ed7246a20d159e2424a088ec2db9841f1083c46
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-07 04:14:16 +0200

    7-add_item.py_0

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+
+list_arg = load_from_json_file("add_item.json")
+for i in sys.argv:
+    list_arg.append(i)
+
+save_to_json_file(list_arg, "add_item.json")
