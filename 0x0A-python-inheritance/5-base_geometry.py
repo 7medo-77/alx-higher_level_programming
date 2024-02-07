90cd48f61d7c980ad8fa6a4d17201caa06ea9cef
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-08 01:49:01 +0200

    5-base_geometry.py_0

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module that defines a class Base Geometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class which forms the base class for different
+    child classes
+    """
+    pass
