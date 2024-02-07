2882676b20bd9fe2ab77d075faa15d375312467b
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-07 02:44:19 +0200

    5-save_to_json_file.py_1 use json.dump instead of json.dumps

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,6 +9,5 @@ def save_to_json_file(my_obj, filename):
     """
     Method that encodes a JSON string and writes it to a file
     """
-    json_text = json.dump(my_obj)
     with open(filename, 'w') as file_1:
-        return(file_1.write(json_text))
+        return(file_1.write(json.dump(my_obj)))
