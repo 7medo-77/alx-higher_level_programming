d41e48d55c3f5b3198f36a5d6d6b164432e57342
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-07 06:13:12 +0200

    7-add_item.py_5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,6 +9,6 @@ def save_to_json_file(my_obj, filename):
     """
     Method that encodes a JSON string and writes it to a file
     """
-    with open(filename, 'w', encoding="utf-8") as file_1:
+    with open(filename, 'w') as file_1:
         json_text = json.dumps(my_obj)
         file_1.write(json_text)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import sys
+import json 
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-with open("add_item.json", 'w') as file:
+try:
     list_arg = load_from_json_file("add_item.json")
-    for i in sys.argv:
-        list_arg.append(i)
-    save_to_json_file(list_arg, "add_item.json")
+except FileNotFoundError:
+    list_arg = []
+
+for i in range(1, len(sys.argv)):
+    list_arg.append(sys.argv[i])
+save_to_json_file(list_arg, "add_item.json")
