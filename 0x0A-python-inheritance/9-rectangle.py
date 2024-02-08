d081d7078a57d10f566bb3b725fd755109cef7d3
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-08 03:29:02 +0200

    9-rectangle.py_0

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module which defines a child class Rectangle which inherits from parent
+class BaseGeometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class with private attributes
+    """
+    def __init__(self, width, height):
+        """Constructor method initializing Rectangle object"""
+        super().integer_validator("height", height)
+        self.__height = height
+        super().integer_validator("width", width)
+        self.__width = width
+
+    def area():
+        """Method which returns the area of Rectangle"""
+        return (self.__height * self.__width)
+
+    def __str__(self):
+        """Method which returns the string representation of class Rectangle"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
