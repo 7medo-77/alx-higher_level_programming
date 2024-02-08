333b25dbfe9e442db60085813e578559dd6b16c2
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-08 02:50:58 +0200

    7-base_geometry.py_5

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -20,8 +20,5 @@ class BaseGeometry:
         """Function that raises exception"""
         if (not isinstance(value, int)):
             raise TypeError("{} must be an integer".format(name))
-        elif (value <= 0):
+        if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
-        else:
-            self.value = value
-            self.name = name
