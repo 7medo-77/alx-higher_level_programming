136b3757847b00867e10eaf068215891d8509856
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-08 04:02:48 +0200

    100-my_int.py_0

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module which defines a class MyInt which is a child
+class of the class int
+"""
+
+
+class MyInt(int):
+    """
+    MyInt child class, which has very peculiar rebelliousness
+    """
+    def __init__(self):
+        """Constructor method inherited from parent int"""
+        super().__init__()
+
+    def __eq__(self, other):
+        """rich comparison method == which is inverted to !="""
+        return (self != other)
+
+    def __nq__(self, other):
+        """rich comparison method != which is inverted to =="""
+        return (self == other)
