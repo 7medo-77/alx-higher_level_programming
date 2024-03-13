a4a61c049bfe1f24ad5a0f51be7f6a4c500a226a
7medo <ahmedabdelhamid1997@gmail.com>
2024-03-12 22:10:41 -0700

    few_fixes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,3 @@
 -- SQL query to collate utf8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE hbtn_0c_0 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- SQL query to display average of all the student scores
-SELECT AVG(score) FROM `second_table`
+SELECT AVG(score) AS average FROM `second_table`
