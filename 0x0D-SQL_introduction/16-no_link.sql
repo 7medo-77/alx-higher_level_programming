bc3d6039918a3a2beb8f02ba2df256b279a40330
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-13 05:43:27 +0200

    16-no_link.sql_1

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- SQL query to display all records of table "second_table"
-SELECT score, name FROM `second_table` WHERE name NOT NULL AND ORDER BY score DESC;
+SELECT score, name FROM `second_table` WHERE name IS NOT NULL AND ORDER BY score DESC;
