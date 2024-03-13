87f5c78dea341a9642d83637f070d7155cf61b08
7medo <ahmedabdelhamid1997@gmail.com>
2024-03-12 21:58:01 -0700

    101-avg_temperatures.sql_3

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Lists all the average temperatures per city by descending order
-SELECT city, SUM(value)/COUNT(Value) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY sum_temp DESC;
+SELECT city, SUM(value)/COUNT(Value) AS avg_temp FROM `temperatures` GROUP BY `city` ORDER BY avg_temp DESC;
