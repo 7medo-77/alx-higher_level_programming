f4dc49a58d383bc1e3d26d6fe0a2e9368ddad90c
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-13 03:52:31 +0200

    4-first_table.sql_8

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,2 @@
--- SQL query to remove a database
--- MySQL 
-CREATE TABLE IF NOT EXISTS `first_table`(
-		`id` INT,
-		`name` VARCHAR(256),
-);
+-- Creates a table called first_table with values id and name in my MySQL server.
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
