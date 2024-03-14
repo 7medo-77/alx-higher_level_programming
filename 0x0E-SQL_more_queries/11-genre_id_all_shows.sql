474e7c995923b0af3f435ebaa75b190ee0318531
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-14 20:44:28 +0200

    11-genre_id_all_shows.sql_4

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,4 @@
 -- Script that lists all shows contained in database 
 -- that have at least one genre liked
 USE `hbtn_0d_tvshows`;
-SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, "NULL") AS `NULL` FROM `tv_shows` FULL JOIN `tv_show_genres` ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, "NULL") AS genre_id FROM `tv_shows` FULL JOIN `tv_show_genres` ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
