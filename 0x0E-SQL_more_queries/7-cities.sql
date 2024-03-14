1e65d6e14c104fc699b69c8b60ae6a207334be1c
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-14 03:26:47 +0200

    7-cities.sql_1

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
 		`id` INT PRIMARY KEY AUTO_INCREMENT,
-		FOREIGN KEY (state_id) REFERENCES state(`state_id`) NOT NULL,
+		`state_id` INT NOT NULL FOREIGN KEY (state_id) REFERENCES state(`state_id`),
 		`name` VARCHAR(256) NOT NULL
 );
