7a998464f30a524288065e43c5076262cbb02138
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-14 03:01:59 +0200

    5-unique_id.sql_0

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- Script that creates a database and a user with some previliges
+-- Script that creates a table
 CREATE TABLE IF NOT EXISTS `id_not_null`(
 		`id` INT NOT NULL,
 		`name` VARCHAR(256)
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- Script that creates a table
+CREATE TABLE IF NOT EXISTS `unique_id`(
+		`id` INT NOT NULL UNIQUE,
+		`name` VARCHAR(256)
+);
