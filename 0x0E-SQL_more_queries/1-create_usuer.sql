565df02bf9541a48dfc81c7c50c592ffcc63dafb
7medo <ahmedabdelhamid1997@gmail.com>
2024-03-13 17:11:49 -0700

    1-create_user.sql_2

---
+++ 0x0E-SQL_more_queries/1-create_usuer.sql
@@ -0,0 +1,3 @@
+-- SQL script that creates a user "user_0d_1" and grants them all previliges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY user_0d_1_pwd;
+GRANT ALL PRIVILIGES ON *.* TO 'user_0d_1'@'localhost';
