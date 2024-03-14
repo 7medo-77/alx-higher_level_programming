f82882b5c0f070efbe45c52b040bda76f5481a91
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-14 03:35:11 +0200

    7-cities.sql_5

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- Script that creates a table
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `cities`(
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(
 		`id` INT PRIMARY KEY AUTO_INCREMENT,
 		`state_id` INT NOT NULL FOREIGN KEY (state_id) REFERENCES state(id),
 		`name` VARCHAR(256) NOT NULL
