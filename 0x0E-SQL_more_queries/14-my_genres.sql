0f90b0a4dbe57009bc06e41ccca77997962fdfca
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 03:45:19 +0200

    14-my_genres.sql_1

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name
 FROM `tv_genres` 
 JOIN `tv_show_genres`
 JOIN `tv_shows`
-ON tv_show_genres.genre_id = tv_genres.id , tv_shows.id = tv_show_genres.show_id
+ON tv_show_genres.genre_id = tv_genres.id AND tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.name = "Dexter";
