82c438d630d47152ae5179c3de26ae69bc367267
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 02:53:50 +0200

    13-count_shows_by_genre.sql_1

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- Script that lists all shows contained in database 
 -- that have at least one genre liked
 USE hbtn_0d_tvshows;
-SELECT tv_show_genres.name, COUNT(tv_show_genres.genre_id) AS `number_of_shows` FROM `tv_show_genres` JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_genres.id DESC;
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS `number_of_shows`
+FROM `tv_genres` JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id ORDER BY tv_genres.id DESC;
