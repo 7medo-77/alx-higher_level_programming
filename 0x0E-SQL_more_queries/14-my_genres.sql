0a808e374cbf91490f79ce466061946c8816a17a
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 03:43:46 +0200

    14-my_genres.sql_0

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Script that lists all shows contained in database 
+-- that have at least one genre liked
+SELECT tv_genres.name
+FROM `tv_genres` 
+JOIN `tv_show_genres`
+JOIN `tv_shows`
+ON tv_show_genres.genre_id = tv_genres.id , tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.name = "Dexter";
