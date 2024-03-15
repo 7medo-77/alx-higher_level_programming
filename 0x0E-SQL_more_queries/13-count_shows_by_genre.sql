d32e3bacfe3063f3be993f9bb286186ddd9d6dd1
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 02:59:58 +0200

    13-count_shows_by_genre.sql_x2

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,4 +2,4 @@
 -- that have at least one genre liked
 USE hbtn_0d_tvshows;
 SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS `number_of_shows`
-FROM `tv_genres` JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id GROUP BY tv_genres.name ORDER BY tv_genres.id DESC;
+FROM `tv_genres` JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id;
