fb76f54a54aef4aff43ea22cc37143134a517297
7medo <ahmedabdelhamid1997@gmail.com>
2024-03-15 07:59:00 -0700

    102-rating_shows.sql_2

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,4 +1,5 @@
 -- Script that lists all shows with their genres
-SELECT title, SUM(tv_show_ratings.rate) FROM tv_shows
+SELECT title, SUM(tv_show_ratings.rate) AS `rating` FROM tv_shows
 LEFT JOIN `tv_show_ratings` ON tv_show_ratings.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
