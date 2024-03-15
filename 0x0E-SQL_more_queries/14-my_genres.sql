12d95ae90ce51a5027fe4a3e257f1e6baf4bba09
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 03:48:02 +0200

    14-my_genres.sql_3

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,7 @@
 -- Script that lists all shows contained in database 
 -- that have at least one genre liked
 USE hbtn_0d_tvshows;
-SELECT tv_genres.name
-FROM `tv_genres` 
-JOIN `tv_show_genres`
-JOIN `tv_shows`
-ON tv_show_genres.genre_id = tv_genres.id AND tv_shows.id = tv_show_genres.show_id
+SELECT tv_genres.name FROM `tv_genres` 
+JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id 
+JOIN `tv_shows` ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.name = "Dexter";
