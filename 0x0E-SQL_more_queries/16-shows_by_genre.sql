6a509a324f830123e21807997a67f0ba0438438d
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 15:58:34 +0200

    16-shows_by_genre.sql_5

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Script that lists all shows with their genres
-SELECT tv_shows.title AS `title`, tv_genres.name AS `name` FROM tv_shows
-LEFT JOIN `tv_show_genres` ON tv_show_genres.show_id = tv_shows.id 
+SELECT tv_shows.title AS `title`, tv_genres.name AS `name` FROM tv_shows, tv_genres
+LEFT JOIN `tv_show_genres` ON tv_show_genres.show_id = tv_shows.id
 JOIN `tv_genres` AS `genres` ON `genres`.id = tv_show_genres.genre_id
 ORDER BY tv_shows.title ASC;
