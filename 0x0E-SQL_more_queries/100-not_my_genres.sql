0c918ccc0db91a2e76788339531764740d39e931
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-03-15 04:46:52 +0200

    100-not_my_genres.sql_1

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,8 +3,8 @@ SELECT tv_genres.name FROM `tv_genres`
 JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id 
 JOIN `tv_shows` ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title != "Dexter"
+ORDER BY tv_genres.name ASC
 WHERE NOT EXISTS(SELECT * FROM 
 SELECT tv_genres.name FROM `tv_genres` 
 JOIN `tv_show_genres` ON tv_show_genres.genre_id = tv_genres.id 
-JOIN `tv_shows` ON tv_shows.id = tv_show_genres.show_id)
-ORDER BY tv_genres.name ASC;
+JOIN `tv_shows` ON tv_shows.id = tv_show_genres.show_id);
