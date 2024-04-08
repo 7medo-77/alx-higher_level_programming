ca6541ae45b5f825f503a5f231299b12745438a7
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-09 01:34:12 +0200

    2-arguments.js_1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-let printOutput = process.argv.length == 3 ? 'Argument found' : 'Arguments found'
-if (process.argv.length >= 3)
-{
-	console.log(printOutput);
-}
-else
-{
-	console.log('No argument');
+const printOutput = process.argv.length === 3 ? 'Argument found' : 'Arguments found';
+
+if (process.argv.length >= 3) {
+  console.log(printOutput);
+} else {
+  console.log('No argument');
 }
