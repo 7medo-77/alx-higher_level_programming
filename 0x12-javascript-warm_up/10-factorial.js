b765bdfdee3db687abb84f5b7e337cef52e34114
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-09 04:18:24 +0200

    10-factorial.js_1

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (a) {
-  if (a === 1 || !a) { 
-    return (1) 
+  if (a === 1 || !a) {
+    return (1);
   }
-  return (a * factorial(a - 1))
+  return (a * factorial(a - 1));
 }
 
 const numOne = parseInt(process.argv[2]);
