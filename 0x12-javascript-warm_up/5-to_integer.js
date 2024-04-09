cffa17633747b4f8aaf9b153770d354758e1a589
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-09 03:20:22 +0200

    5-to_integer.js_2

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 if (process.argv[2]) {
   const stringOne = parseInt(process.argv[2]);
-  const outputString = 'My number: ' + stringOne;
+  const outputString = stringOne ? 'My number: ' + stringOne : 'Not a number';
   console.log(outputString);
 } else {
   console.log('Not a number');
