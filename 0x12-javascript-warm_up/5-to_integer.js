55a83836a071074e3324e16ab68369ba97a6aed5
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-09 03:17:29 +0200

    5-to_integer.js_0

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv[2]) {
+  const stringOne = parseInt(process.argv[2]);
+  const outputString = 'My number: ' + stringOne;
+  console.log(outputString);
+} else {
+  console.log('Not a Number');
+}
