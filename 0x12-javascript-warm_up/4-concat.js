33fa4264d7cb0277f04ee760c0151f2dcdabec87
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-09 02:54:41 +0200

    4-concat.js_1

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let string_1 = process.argv[2] ? process.argv[2] : 'undefined';
-let string_2 = process.argv[3] ? process.argv[3] : 'undefined';
-let outputString = string_1 + ' is ' + string_2;
+const stringOne = process.argv[2] ? process.argv[2] : 'undefined';
+const stringTwo = process.argv[3] ? process.argv[3] : 'undefined';
+const outputString = stringOne + ' is ' + stringTwo;
 
 console.log(outputString);
