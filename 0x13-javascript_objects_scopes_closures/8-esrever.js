7233026b2c313f799c54d9e0fe5af10dd6457635
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-10 05:35:19 +0200

    8-esrever.js_3

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let i;
-  let j;
-  let newArray = list;
+  let i, j = list.length - 1;
+  let newArray = list.slice();
 
-  for (i = 0, j = newArray.length - 1; i >= j; i++, j--) {
+  for (i = 0; i <= j; i++, j--) {
     let tempVariable;
 
     tempVariable = newArray[i];
-    newArray[i] = list[j];
+    newArray[i] = newArray[j];
     newArray[j] = tempVariable;
   }
   return (newArray);
