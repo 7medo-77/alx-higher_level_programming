75ad763dc3debd69fc7dde4d302fc46d1736a842
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-10 03:18:46 +0200

    3-rectangle.js_0

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w && h) && (w > 0 && h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print(){
+    for (let i = 0; i < this.height; i++){
+      let printArray = '';
+      for (let j = 0; j < this.width; j++){
+        printArray += 'X'
+      }
+      console.log(printArray);
+    }
+  }
+
+}
+module.exports = Rectangle;
