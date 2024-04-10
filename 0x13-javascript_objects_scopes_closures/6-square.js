3537a451ba5a37e3951d3e6dbebedbb20977e2e7
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-10 04:23:48 +0200

    6-square.js_2

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,19 +2,14 @@
 const SquareParent = require('./5-square.js');
 
 class Square extends SquareParent {
-  constructor(size) {
-    super(size);
-  }
-
   charPrint (c) {
     for (let i = 0; i < this.height; i++) {
       let printArray = '';
       for (let j = 0; j < this.width; j++) {
-        printArray += c ? c : 'X';
+        printArray += !c ? 'X' : c;
       }
       console.log(printArray);
     }
   }
-
 }
 module.exports = Square;
