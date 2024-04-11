f9eb668f48a12aa3b652e7486b1374ea5626ad67
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-11 23:49:35 +0200

    11-second_biggest.js_4

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,14 +6,14 @@ for (let i = 2; i < process.argv.length; i++) {
   myArray.push(argNum);
 }
 
-let maxNumber = Number.MIN_SAFE_INTEGER;
-let secondMaxNumber = Number.MIN_SAFE_INTEGER;
+let maxNumber = myArray[0];
+let secondMaxNumber = myArray[0];
 
 for (const number of myArray) {
   if (number > maxNumber) {
     maxNumber = number;
   }
-  if (number >= secondMaxNumber && number < maxNumber) {
+  if ((number >= secondMaxNumber && number < maxNumber) || (secondMaxNumber === maxNumber && number === myArray[0])) {
     secondMaxNumber = number;
   }
 }
