70e3d69bc65cea0db7904610de203351eee49345
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-11 06:55:40 +0200

    102-concat.js_0

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
-console.log(process.argv[0])
+const fs = require('fs');
+
+fs.readFile(process.argv[3], (err, data) => {
+  const contentFile2 = data.toString();
+});
+
+fs.writeFile(process.argv[2], contentFile2, 'a', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
