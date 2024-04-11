b23c663d263c5e3b2a892dce700aaf68c918700e
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-11 06:33:38 +0200

    101-sorted.js_2

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,10 +3,9 @@ const dict = require('./101-data.js').dict;
 
 function dictConstructor (dict) {
   const uniqueValues = new Set(Object.values(dict));
-  const valuesArray = [...uniqueValues];
   const returnObject = {};
 
-  for (const number of valuesArray) {
+  for (const number of uniqueValues) {
     const elementArray = [];
     for (const key in dict) {
       if (dict[key] === number) {
