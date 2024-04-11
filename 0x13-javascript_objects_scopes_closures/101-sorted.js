28dcd1df809f269f112de7ec4ae571c4fc79df6f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-11 06:17:44 +0200

    101-sorted.js_1

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
-let dict = require('./101-data.js').dict;
+const dict = require('./101-data.js').dict;
 
-function dictConstructor(dict) {
-  let uniqueValues = new Set(Object.values(dict));
+function dictConstructor (dict) {
+  const uniqueValues = new Set(Object.values(dict));
   const valuesArray = [...uniqueValues];
   const returnObject = {};
 
   for (const number of valuesArray) {
-    let elementArray = [];
+    const elementArray = [];
     for (const key in dict) {
       if (dict[key] === number) {
         elementArray.push(`${key}`);
-        returnObject[`${number}`] = elementArray
+        returnObject[`${number}`] = elementArray;
       }
     }
   }
   console.log(returnObject);
 }
+
 dictConstructor(dict);
