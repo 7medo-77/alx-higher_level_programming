00eb55dee4eac63c11689e9d40ac6f960f7f141d
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-14 07:34:41 +0200

    5-filter_cities.py_07:34

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     query = """
         SELECT cities.name FROM cities
         JOIN states ON states.id = cities.state_id
-        WHERE cities.name = {}
+        WHERE cities.name = '{}'
         ORDER BY cities.id ASC;
     """.format(arg)
 
