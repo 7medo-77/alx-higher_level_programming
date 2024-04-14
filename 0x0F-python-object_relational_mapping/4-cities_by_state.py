f70c941a3161b6a3c74b9fb617e4f8de4c0cb0d1
7medo <ahmedabdelhamid1997@gmail.com>
2024-04-13 20:40:02 -0700

    4-cities_by_state.py_0

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Python script which selects some states from table states"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            password=argv[2],
+            database=argv[3],
+        )
+    cur = conn.cursor()
+    arg = argv[4].split()[0]
+    query = """
+        USE hbtn_0e_4_usa
+        SELECT cities.id, cities.name, states.name FROM cities
+        JOIN states ON states.name = cities.state_id
+        ORDER BY cities.id ASC
+    """
+    cur.execute(query)
+
+    res_words = cur.fetchall()
+    for word in res_words:
+        print(word)
+
+    cur.close()
+    conn.close()
