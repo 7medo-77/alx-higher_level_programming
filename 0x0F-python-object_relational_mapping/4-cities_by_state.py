4cb27fc9e02dc1f7ce9b116e56d03e4102430494
7medo <ahmedabdelhamid1997@gmail.com>
2024-04-13 20:53:41 -0700

    4-cities_by_state.py 20:53

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,10 +14,9 @@ if __name__ == "__main__":
             database=argv[3],
         )
     cur = conn.cursor()
-    arg = argv[4].split()[0]
     query = """
         USE hbtn_0e_4_usa
-        SELECT cities.id, cities.name, states.name FROM cities
+        SELECT cities.id, cities.name, cities.state_id FROM cities
         JOIN states ON states.name = cities.state_id
         ORDER BY cities.id ASC
     """
