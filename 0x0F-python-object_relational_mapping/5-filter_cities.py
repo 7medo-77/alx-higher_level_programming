de809b8359416284c4e08fe6bacb0bc84760b8fc
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-14 08:05:28 +0200

    5-filter_cities.py_08:05

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -22,13 +22,7 @@ if __name__ == "__main__":
         ORDER BY cities.id ASC""".format(arg)
 
     cur.execute(query)
-
-    # res_words = cur.fetchall()
-
     print(", ".join(x[0] for x in cur.fetchall()))
-    # for index, word in enumerate(res_words):
-    #     print("{}".format(word[0])) if index == len(res_words) - 1\
-    #                         else print("{}, ".format(word[0]), end="")
 
     cur.close()
     conn.close()
