a3813492d89c2363bc86dec65de033444c31a2db
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-14 06:34:29 +0200

    5-filter_cities.py_06:34

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     cur = conn.cursor()
     # arg = argv[4].split()[0]
     query = """
-        SELECT cities.name FROM cities
+        SELECT states.name, cities.name FROM cities
         JOIN states ON states.id = cities.state_id
         GROUP BY states.name
-        ORDER BY cities.name
+        ORDER BY cities.name;
     """
 
     cur.execute(query)
