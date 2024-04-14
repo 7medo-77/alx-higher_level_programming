7005b660b60c58ca9519a9640ebe95f024d44b70
7medo <ahmedabdelhamid1997@gmail.com>
2024-04-13 17:15:50 -0700

    1-filter_states.py_2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,20 @@
 import MySQLdb
 from sys import argv
 
-conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        password=argv[2],
-        database=argv[3],
-    )
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE `name` LIKE 'N%'")
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            password=argv[2],
+            database=argv[3],
+        )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE `name` LIKE 'N%'")
 
-n_words = cur.fetchall()
+    n_words = cur.fetchall()
 
-for word in n_words:
-    print(word)
-cur.close()
-conn.close()
+    for word in n_words:
+        print(word)
+    cur.close()
+    conn.close()
