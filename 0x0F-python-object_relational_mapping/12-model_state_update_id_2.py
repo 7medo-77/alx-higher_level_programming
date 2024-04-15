afa6958f1507de3853713d0cf70d6d6fcd960c60
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-15 09:21:09 +0200

    12-model_state_update_id_2.py_09:21

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    res_obj = session.query(State).filter(State.id == 2)
+    res_obj.name = "New Mexico"
+
+    session.commit()
+    session.close()
+
