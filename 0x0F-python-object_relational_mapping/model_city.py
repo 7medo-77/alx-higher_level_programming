4563ee50fb0bdbed2c07ed3431e29e2bfd03f39b
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-16 03:35:51 +0200

    model_city.py_03:35

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -18,7 +18,7 @@ class City(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     state = relationship("State", backref='states')
 
     def __repr__(self):
