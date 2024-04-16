4cee5f83d770f0f77e2ed1c6c479711008d28cfc
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-16 06:00:17 +0200

    relationship_city.py_06:00

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -6,6 +6,7 @@ class State and connects class to database
 
 
 from relationship_state import Base, State
+from sqlalchemy.orm import relationship
 from sqlalchemy import String, Integer, Column, ForeignKey
 
 
@@ -16,6 +17,7 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship('State', backref='cities')
 
     def __repr__(self):
         rep = "{}: {}".format(self.id, self.name)
