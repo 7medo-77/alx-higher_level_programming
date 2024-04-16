3fa1068baa24e85b43b49d34e798e6b3415eb689
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-16 03:28:19 +0200

    model_city.py_03:28

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,7 +6,7 @@ class State and connects class to database
 
 
 from sqlalchemy import String, Integer, Column, ForeignKey
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -19,7 +19,7 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
-    state = relationship("State", backref='states')
+    # state = relationship("State", backref='states')
 
     def __repr__(self):
         rep = "{}: ({}) {}".format(self.state_id, self.id, self.name)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@ class State and connects class to database
 
 from sqlalchemy import String, Integer, Column
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -18,7 +18,7 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='states')
+    # cities = relationship("City", backref='states')
 
     def __repr__(self):
         rep = "{}: {}".format(self.id, self.name)
