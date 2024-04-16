bc780fd42d077ce9804cbd1e4f6af5ad41cb84e1
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-16 03:21:46 +0200

    model_state.py_03:21

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,21 +5,22 @@ class State and connects class to database
 """
 
 
-from sqlalchemy import String, Integer, Column
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import String, Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class State(Base):
+class City(Base):
     """State class"""
-    __tablename__ = "states"
+    __tablename__ = "cities"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref='states')
+    state_id = Column(Integer, ForeignKey('states.id'))
+    state = relationship("State", backref='states')
 
     def __repr__(self):
-        rep = "{}: {}".format(self.id, self.name)
+        rep = "{}: ({}) {}".format(self.state_id, self.id, self.name)
         return (rep)
