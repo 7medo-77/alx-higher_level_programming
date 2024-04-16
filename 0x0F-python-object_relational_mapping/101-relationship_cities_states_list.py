a046df0d99e0ddecf18fc6c7107d6bb78009c528
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-16 05:57:45 +0200

    101-relationship_cities_states_list.py_05:57

---
+++ 0x0F-python-object_relational_mapping/101-relationship_cities_states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(bind=engine)
+
+    # State.relationship("City", backref="states", order_by=City.id)
+    results = session.query(City).order_by(City.id)
+
+    for state in results.all():
+        print("{}: {} -> {}".format(state.id, state.name, state.state))
+
+    session.close()
