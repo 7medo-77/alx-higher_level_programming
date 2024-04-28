3868c333f93c063a8e4408b02d47f5ecc81a4891
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-28 16:36:27 +0300

    0-body_size.sh_16:36

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script which displays the size of the body of the HTTP response
-curl -I "$1" | grep 'Length' | cut -d ":" -f 2 | sed "s/\s//g"
+curl -sI "$1" | grep 'Length' | cut -d ":" -f 2 | sed "s/\s//g"
