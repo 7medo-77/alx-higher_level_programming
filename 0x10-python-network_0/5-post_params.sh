ca7252b854b7f1f41da27af7ba67424c3ff6e489
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-28 18:34:00 +0300

    5-post_params.sh_18:34

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script sends a GET request along with a custom header variable
-curl -ds -X POST "email=test@gmail.com&subject=I%will%always%be%here%for%PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I%will%always%be%here%for%PLD" "$1"
