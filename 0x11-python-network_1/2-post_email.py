479e1aa6812dd9f4ba59acee5cb7be6f089866c9
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 14:16:16 +0300

    2-post_email.py_14:16

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script which fetches a link and returs the HTTP response body"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    values = {"email": sys.argv[2]}
+    package = urllib.parse.urlencode(values)
+    response_obj = urllib.request.Request(sys.argv[1], data=package)
+
+    with urllib.request.urlopen(response_obj) as request:
+        response = request.read().decode('utf-8')
+    print(response)
