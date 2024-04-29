bb8adaa1a40324a41b924a12a78b7255fc3ad26f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 19:53:58 +0300

    7-error_code.py_19:53

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script which fetches a link and returs the HTTP response body"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.text)
+    except requests.exceptions.HTTPError as e:
+        print(e.errno)
