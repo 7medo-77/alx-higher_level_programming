54f1c5ab07aa5947c959155a0ef0d25b1031fadd
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 18:26:22 +0300

    5-hbtn_header.py_18:26

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Python script which fetches a link and returs the HTTP response body"""
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    r = requests.get(sys.argv[1])
     print(r.headers['X-Request-Id'])
