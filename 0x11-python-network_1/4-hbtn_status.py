02a146312e24b1a04fe4c3060dda79822ededf14
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 16:08:29 +0300

    4-hbtn_status.py_16:08

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,12 @@ import requests
 
 
 if __name__ == "__main__":
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    print("""Body response:
-    \t- type: {}
-    \t- content: {}""".format(type(response.text), response.text))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
+    
+    # response = requests.get('https://alx-intranet.hbtn.io/status')
+    # print("""Body response:
+    # \t- type: {}
+    # \t- content: {}""".format(type(response.text), response.text))
