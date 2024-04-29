329ed23a6e2dbac9cf8cbffac60a4a134f436a56
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 19:30:07 +0300

    6-post_email.py_19:30

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script which fetches a link and returs the HTTP response body"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {"email" : sys.argv[2]}
+    r = requests.post(sys.argv[1], data)
+    print(r.text)
