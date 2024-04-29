ad82733e93e70cc2dd4132c45c325812a0bb0fc9
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 20:32:04 +0300

    8-json_api.py_20:32

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     param = {"q": sys.argv[1] if sys.argv[1] else ""}
     response = requests.post('http://0.0.0.0:5000/search_user', param)
-    print(response.json())
+    print(response.json()['id'])
