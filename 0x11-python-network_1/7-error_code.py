5af15bc6addbb41178d5c0814c23f6e7b16a61b3
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 19:59:36 +0300

    7-error_code.py_19:59

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         r = requests.get(sys.argv[1])
         print(r.text)
     except requests.exceptions.HTTPError:
-        print(r.status_code)
+        print("Error code: {}".format(r.status_code))
