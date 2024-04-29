a17a2360bf1ac0554590b3f85d2c154d508484b4
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 14:34:11 +0300

    2-post_email.py_14:34

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     values = {"email": sys.argv[2]}
-    package = urllib.parse.urlencode(values)
+    package = urllib.parse.urlencode(values).encode("ascii")
     response_obj = urllib.request.Request(sys.argv[1], data=package)
 
     with urllib.request.urlopen(response_obj) as request:
