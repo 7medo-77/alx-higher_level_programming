12b7284230ea86443baaca1217af55530bf04624
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 19:47:53 +0300

    6-post_email.py_19:47

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     data = {"email" : sys.argv[2]}
     r = requests.post(sys.argv[1], data)
-    print("Your email is: {}".format(r.content))
+    print(r.text)
