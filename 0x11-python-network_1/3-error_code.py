c20e0fc39bacfa29d43e040eae21fc916809e84f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-04-29 15:21:54 +0300

    3-error_code.py_15:21

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Python script which fetches a link and returs the HTTP response body"""
-import urllib.request
-import urllib.error
+import urllib.request as request
+import urllib.error as error
 import sys
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(sys.argv[1]) as request:
+    with request.urlopen(sys.argv[1]) as request:
         try:
             response = request.read().decode('utf-8')
             print(response)
-        except urllib.error.HTTPError as e:
+        except error.HTTPError as e:
             print("Error code: {}".format(e.code))
