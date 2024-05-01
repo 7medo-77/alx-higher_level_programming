d5dd3fa34541307392b2e1ef4a3870ea95d76ee7
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-01 18:27:29 +0300

    102-catch_me.sh_18:27

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script sends a PUT request along with a custom header variable
-curl -H "Content-Type: text/plain" -X PUT "You got me!" "$1"
+curl  -X PUT "You got me!" -H "Content-Type: text/plain" "$1"
