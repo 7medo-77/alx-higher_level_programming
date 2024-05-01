a1bf7d5cb0dd4325ad9aacca5ce0e580f0582229
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-01 17:53:30 +0300

    101-post_json.sh_17:53

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script sends a GET request along with a custom header variable
-curl -H "Content-Type: application/json" --data-raw @"$2" "$1"
+curl -H "Content-Type: application/json" --data @"$2" "$1"
