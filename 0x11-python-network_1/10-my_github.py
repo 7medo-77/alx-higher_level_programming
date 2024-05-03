83c5f792b2f6b665c975a891587523dda4920fca
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-03 16:16:07 +0300

    10-my_github.py_16:16

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     passwd = sys.argv[2]
     auth_tuple = (username, passwd)
-    # headers = {'Authorization': passwd}
     response = requests.request("GET",
                                 'https://api.github.com/user', auth=auth_tuple)
-    # print(json.dumps(response.json(), indent=4))
     print(response.json().get('id'))
