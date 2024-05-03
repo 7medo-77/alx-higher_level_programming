f4b4cd99613d1fa999fc2a33ac75e693dc7c0b36
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-03 16:55:51 +0300

    100-github_commits.py_16:55

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,17 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url_string = "https://api.github.com/repos/" + owner_name + '/' + repo_name + "commits"
+    url_string = "https://api.github.com/repos/" + owner_name + '/' \
+        + repo_name + '/' + "commits"
     # auth_tuple = (username, passwd)
     response = requests.request("GET",
                                 url_string,)
-    print(response.json().get('sha'))
+    list_json = []
+    for index, response in enumerate(response.json()[0:10]):
+        tuple_json = (response['sha'], response.get("commit")
+                      .get("author").get("name"))
+        list_json.append(tuple_json)
+    for index, element in enumerate(list_json):
+        print("{} : {}".format(element[0], element[1]))
+    # print(list_json)
+    # print(response.json()[0:9])
