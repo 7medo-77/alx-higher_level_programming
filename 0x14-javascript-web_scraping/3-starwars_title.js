714a5113a6f7446fa072f1b67265d03e29e3fed8
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-22 04:11:14 +0300

    output.json_04:11

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const apiCall = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(apiCall, (error, response, body) => {
+  if (error) {
+    console.log(response.error);
+  }
+  jsonReply = JSON.parse(response.body);
+  console.log(jsonReply.title);
+});
