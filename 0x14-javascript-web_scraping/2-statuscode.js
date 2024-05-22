84ef8934a96a982a70225d0de6213b4de6ae5186
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-22 03:35:01 +0300

    2-statuscode.js_03:35

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,11 +3,9 @@ const request = require('request');
 const process = require('process');
 const websiteName = process.argv[2];
 
-const requestLink = request.Request(websiteName)
-
 request(websiteName, (error, response, body) => {
   if (error) {
     console.log(error);
   }
-  console.log(response.statusCode);
+  console.log("code: ", response.statusCode);
 })
