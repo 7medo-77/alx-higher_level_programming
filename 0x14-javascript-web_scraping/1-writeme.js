166e7954292a0acfc0a884a16044f863ff48d505
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-22 03:21:51 +0300

    1-writeme.js_03:21

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 const process = require('process');
-const file_name = process.argv[2];
+const fileName = process.argv[2];
 const data = process.argv[3];
 
-fs.writeFile(file_name, data,'utf-8', (err) => {
+fs.writeFile(fileName, data, 'utf-8', (err) => {
   if (err) {
     console.log(err);
-    return;
   }
 });
