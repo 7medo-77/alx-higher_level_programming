5ff33585f855094f4d575f5ea4bacd9fa5efbbee
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-22 04:34:46 +0300

    4-starwars_count.js_04:34

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const process = require('process');
+const apiCall = process.argv[2];
+const wedgeAntilles = "https://swapi-api.alx-tools.com/api/people/" + "18/"
+
+request(apiCall, (error, response, body) => {
+  if (error) {
+    console.log(response.error);
+  }
+
+  let num = 0;
+  const jsonReply = JSON.parse(response.body);
+  for (res of jsonReply.results){
+    if (res.characters.find(character => character === wedgeAntilles)){
+      num++;
+    }
+  }
+  console.log(num);
+});
