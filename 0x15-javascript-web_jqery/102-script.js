ea04afcbe473d7e0f3172a7c0a285d8283874e9b
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-31 17:11:59 +0300

    102-script.js_17:11

--- 0x15-javascript-web_jqery/102-script.js
+++ 0x15-javascript-web_jqery/102-script.js
@@ -1,4 +1,20 @@
 $(document).ready(() => {
+
+    $('#language_code').on("focus keydown", (event) => {
+        const lang = $('#language_code').val();
+        const request = "https://hellosalut.stefanbohacek.dev/?lang=" + lang;
+
+        if (event.keyCode === 13)
+            $.get({
+                type: 'GET',
+                url: request,
+                success: (response) => {
+                    const result = response.hello;
+                    $('#hello').text(result);
+                },
+            })
+    })
+
     $('#btn_translate').on("click", () => {
         const lang = $('#language_code').val();
         const request = "https://hellosalut.stefanbohacek.dev/?lang=" + lang;
@@ -12,4 +28,5 @@ $(document).ready(() => {
             },
         })
     })
+
 });
